0e9fdadb9945026082281f331ed8aa3b96dcadcc
SZH0728 <sun060728@126.com>
2024-07-24 16:47:25 +0800

    Optimize database connection pool settings for better stability
    Update the database connection pool configuration in dbmanager.py to
    include 'pool_pre_ping=True' alongside 'pool_recycle=7200'. This change
    aims to enhance the database connection stability by periodically checking
    the connection's liveness before reusing it from the pool.
    
    Reference: https://docs.sqlalchemy.org/en/14/core/pooling.html#sqlalchemy.pool.PoolPrePing

--- dbmanager.py
+++ dbmanager.py
@@ -24,7 +24,7 @@ DB_URI = f'mariadb+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}'
 Base = declarative_base()
 
 # 创建数据库引擎
-Engine = create_engine(DB_URI, pool_recycle=28000)
+Engine = create_engine(DB_URI, pool_recycle=7200, pool_pre_ping=True)
 
 # 创建sessionmaker绑定到数据库引擎
 Session = sessionmaker(bind=Engine, autoflush=False)
