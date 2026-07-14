55fe8bfa44125e264cbc2a25a1fc2b6855644c77
SZH0728 <sun060728@126.com>
2026-07-14 14:31:32 +0800

    feat(scheduler): 配置 Bangumi 每日定时采集任务

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -7,9 +7,33 @@
          main.py 从此处导入并传入 Scheduler 构造函数。
 """
 
+from datetime import time
+
+import httpx
+
+from config import config
+from data.request import SingleHttpxRequestData
 from scheduler.base import Scheduler, SiteScheduleConfig
 
-SCHEDULE_REGISTRY: tuple[SiteScheduleConfig, ...] = tuple()
+SCHEDULE_REGISTRY: tuple[SiteScheduleConfig, ...] = (
+    SiteScheduleConfig(
+        site_name='bangumi',
+        trigger_times=(time(
+            config.getint('bangumi', 'trigger_hour'),
+            config.getint('bangumi', 'trigger_minute'),
+        ),),
+        seed_tasks=(
+            SingleHttpxRequestData(
+                retry=config.getint('bangumi', 'retry'),
+                request=httpx.Request(
+                    'GET',
+                    'https://api.bgm.tv/calendar',
+                    headers={'User-Agent': config.get('bangumi', 'user_agent')},
+                ),
+            ),
+        ),
+    ),
+)
 
 if __name__ == '__main__':
     pass
